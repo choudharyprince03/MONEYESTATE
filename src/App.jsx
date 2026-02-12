b15ef604802acc25e2b8ab15fc7685d2af6593c2
Prince Choudhary <prince.1978choudhary@gmail.com>
2026-02-12 18:13:17 +0530

    removed features

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landingpage from "./screens/landingpage";
-import Feature from "./screens/feature";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element = {<Landingpage/>} />
-        <Route path="/Feature" element = {<Feature/>} />
       </Routes>
     </Router>
 
